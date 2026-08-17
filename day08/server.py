80fdbdd8ad7cd0ca13a84337f5e2283fbe15a14f
venkateshA-cmd <venkia.venkatesh07@gmail.com>
2026-08-17 14:45:54 +0530

    Day 10: Configured CORSMiddleware for production frontend integration

--- day08/server.py
+++ day08/server.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_headers=["*"],
+    allow_methods=["*"]
+)
 
 # Endpoint 1: Health Check
 @app.get("/")
